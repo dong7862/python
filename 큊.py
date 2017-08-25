d09219002e9087052ae3e378134d0a0f6b629e5b
dong7862 <dong7862@naver.com>
2017-08-25 21:39:06 +0900

    Update 큊.py

--- 큊.py
+++ 큊.py
@@ -3,16 +3,28 @@ import time
 print("당신의 수학능력을 평가해보죠.")
 randombox=range(100)
 qwer=('+', '-', 'x')
-print("\n시작을 입력해주세요\n")
+print("\n문제를 풀어주세요.\n")
+print("※주의 사항※\n■숫자가 아닌 다른것을 입력하면 자동종료됩니다.■")
 
 question_num=1
 while(True):
-    a=input("입력 :")
-    if a=='시작':
-        x=random.choice(randombox)
-        z=random.choice(randombox)
-        y=random.choice(qwer)
-        print("\n\n%d번 문제 :%s %s %s"%(question_num, x, y, z))
-        question_num=question_num+1
+    #문제 생성
+    x=random.choice(randombox)
+    y=random.choice(qwer)
+    z=random.choice(randombox)
+    print("\n\n%d번 문제 :%s %s %s ="%(question_num, x, y, z))
+    a=int(input("입력 :"))
+    #정답 판별
+    if y=='+':
+        za=x+z
+    elif y=='-':
+        za=x-z
+    elif y=='x':
+        za=x*z
+    if a==(za):
+        print("\n정답")
     else:
-        print("\n시작을 입력 하라고\n")
+        print("\n오답")
+        str(za)
+        print("\n답 :",za)
+    question_num=question_num+1
